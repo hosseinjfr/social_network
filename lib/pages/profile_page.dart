8e9fc31c078db7fa8ca189edef254b355dc92eb9
hosseinjfr <hosseinjfr72@gmail.com>
2022-04-20 18:50:00 +0430

    Finished

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:social_network/data/me_post_json.dart';
 import 'package:social_network/theme/colors.dart';
+import 'package:video_player/video_player.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -9,6 +12,23 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  bool isPhoto = true;
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = VideoPlayerController.network(meVideoList[0]['videoUrl']);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +37,7 @@ class _ProfilePageState extends State<ProfilePage> {
         child: getAppBar(),
         preferredSize: Size.fromHeight(200),
       ),
+      body: getBody(),
     );
   }
 
@@ -71,5 +92,194 @@ class _ProfilePageState extends State<ProfilePage> {
         ),
       ),
     );
+  }
+
+  Widget getBody() {
+    var size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 25, right: 25),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      'Posts',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: black,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      '35',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: black,
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      'Followers',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: black,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      '1,552',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: black,
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      'Follow',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: black,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      '182',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: black,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isPhoto = true;
+                    });
+                  },
+                  icon: Icon(
+                    Foundation.photo,
+                    size: 25,
+                    color: isPhoto ? primary : black,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isPhoto = false;
+                    });
+                  },
+                  icon: Icon(
+                    Foundation.play_video,
+                    size: 30,
+                    color: !isPhoto ? primary : black,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            isPhoto
+                ? Wrap(
+                    spacing: 20,
+                    runSpacing: 20,
+                    children: List.generate(mePostList.length, (index) {
+                      return Container(
+                        width: (size.width - 70) / 2,
+                        height: (size.height - 70) / 2,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          image: DecorationImage(
+                              image: NetworkImage(mePostList[index]),
+                              fit: BoxFit.cover),
+                        ),
+                      );
+                    }),
+                  )
+                : Wrap(
+                    spacing: 20,
+                    runSpacing: 20,
+                    children: List.generate(meVideoList.length, (index) {
+                      return GestureDetector(
+                        onTap: () {
+                          playVideo(context, meVideoList[index]['videoUrl']);
+                        },
+                        child: Container(
+                          width: (size.width - 70) / 2,
+                          height: (size.height - 70) / 2,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            image: DecorationImage(
+                                image: NetworkImage(meVideoList[index]['img']),
+                                fit: BoxFit.cover),
+                          ),
+                          child: Center(
+                            child: Icon(
+                              AntDesign.playcircleo,
+                              size: 60,
+                              color: white,
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+                  ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  playVideo(BuildContext context, videoUrl) {
+    _controller = VideoPlayerController.network(videoUrl);
+    _controller.addListener(() {
+      setState(() {});
+    });
+
+    _controller.setLooping(true);
+    _controller.initialize().then((_) {
+      // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+      setState(() {});
+    });
+    _controller.play();
+
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              contentPadding: EdgeInsets.zero,
+              content: AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
+              ),
+            ));
   }
 }
